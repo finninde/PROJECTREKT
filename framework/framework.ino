28f6c38d15b5057fb1d165272d936d25ba93be9d
Sigmund Eggen Holm <Sigmund Eggen Holm>
2016-03-07 12:59:48 +0100

    refractored directory so that arduino.cc doesnt ruin it

---
+++ framework/framework.ino
@@ -0,0 +1,59 @@
+#define aggressive 1
+#define defensive 2
+#define search 3
+//Sensor struct, contains all sensory input
+struct sensors{
+  float UVLeft;
+  float UVRight;
+  float GyroX;
+  float GyroY;
+  float GyroZ;
+  }
+
+//behavior struct, add points towards a behavior
+struct behaviors{
+  int Agressive;
+  int Defensive;
+  int Search;
+  }
+
+void updateAllSensors(){
+  //Updates all sensors in sensors struct
+  }
+
+int evaluate(){
+  // Iterates sensors and returns a behavior it takes aggressive, 
+  // defensive or search as arguments
+  
+  }
+
+int runBehavior(behavior, runtime){
+  // Runs a behavior for a runtime
+  // Returns 1 if successfull and 0 if interrupted
+  return 1;
+  }
+
+void setup() {
+  //TODO: initialize sensors by running update
+  updateAllSensors();
+  
+}
+
+void loop() {
+  //Update sensors like they should be
+  updateAllSensors();
+  //Evaluate and decide on a behavior
+  behavior = evaluate();
+  // set a runtime for selected behavior
+  runtime = random(0,2);
+  // run the behavior
+  if (runBehavior(behavior, runtime)){
+      // behavior ran and terminated
+      
+    }
+  else{
+      // behavior ran and should not be run again
+      
+    }  
+
+}
